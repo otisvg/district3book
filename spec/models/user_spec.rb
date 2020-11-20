5a7206fdfe61cd53441db37362ab6a883bda3846
Otis <otisvickersgraver@gmail.com>
2020-11-20 11:41:13 +0000

    sign in and sign out feature tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
    describe "Users can register" do
     it "checks that a user can registered" do
-       user = User.new(username: "example", email: "example@example.com", password: "123456", password_confirmation: "123456")
-
+      user = User.new(username: "example", email: "example@example.com", password: "123456", password_confirmation: "123456")
       expect(user.username).to eq "example"
     end
   end
