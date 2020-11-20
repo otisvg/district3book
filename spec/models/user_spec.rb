70a96a63d0d2b22e3c31a7d23fb8c606bdff4713
Beca-g <rebekahgalliano@gmail.com>
2020-11-20 10:45:00 +0000

    Feat: Passed unit test to check that a user can sign up.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
    describe "Users can register" do
     it "checks that a user can registered" do
-      User.new(username: "example", email: "example@example.com", password: "123456", password_confirmation: "123456")
-      expect(User.find_by(username: "example")).to be
+       user = User.new(username: "example", email: "example@example.com", password: "123456", password_confirmation: "123456")
+
+      expect(user.username).to eq "example"
     end
   end
 end
