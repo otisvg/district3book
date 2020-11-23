95fda045cc2ffea96a6952c8fec4a689d36f4a39
Beca-g <rebekahgalliano@gmail.com>
2020-11-23 16:45:50 +0000

    Created serializer controllers and homepage

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
    root to: "posts#index"
   resources :posts
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :posts, only: [:create, :edit, :destroy]
+    end
+  end
+
+  get '#path', to: 'homepage#index', via: :all          
 end
