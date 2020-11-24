593ecf150cc29cf6e3823aaa6f969f2c556f8f42
Beca-g <rebekahgalliano@gmail.com>
2020-11-24 11:15:33 +0000

    Started basic styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201119160016) do
+ActiveRecord::Schema.define(version: 2020_11_19_160016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
