ae3a89bcb95fc1ca3632e7090ee87f5d62b364db
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-25 16:33:53 +0000

    Adding routes on v1 to create posts

--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -3,6 +3,16 @@ module API
     class Posts < Grape::API
       include API::V1::Defaults
       resource :posts do
+
+        desc "Creates a new post"
+       params do 
+        requires :message, type: String, desc: "message of the post"
+        requires :user_id, type: String, desc: "Username of current user"
+       end 
+       post "message" do
+        Post.create(message: permitted_params[:message], user_id: permitted_params[:user_id])
+       end 
+
         desc "Return all posts"
         get "" do
           Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :posts
   mount API::Base, at: "/"
 
   get '#path', to: 'homepage#index', via: :all
